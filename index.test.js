0f74ed16ebb7b8570331ea8708f932753018ef73
Vincent Sanders <vincesanders@hotmail.com>
2020-04-27 16:03:20 -0500

    Made some changes to the knexfile.

--- index.test.js
+++
@@ -1,9 +0,0 @@
-/**
- * @jest-environment node
- */
-//TODO more robust testing
-describe('index.js file', () => {
-    test('should run the test', function() {
-        expect(true).toBe(true);
-    });
-});
